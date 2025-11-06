f41bcaea14950c617b9696e87386c107d31a2a8a
Janani-1104 <jananiravichandran343@gmail.com>
2025-11-06 15:48:52 +0530

    spring boot

---
+++ AdminService_Janani/src/main/java/com/tnsif/adminservice/AdminServiceJananiApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceJananiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceJananiApplication.class, args);
+	}
+
+}
